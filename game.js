aab26d03c21c2f26c804d04c35d1a554c863ad6f
Mohammed Musharaf <mohamedmusharaf687@gmail.com>
2025-06-24 19:39:33 +0530

    cool transition ans states

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ const ctx = canvas.getContext("2d");
 let lt = 0;
 let fullscreenchanged = false;
 let clicked = false;
+let timer = 0;
+let transition = 0;
+let state = 0;
+
 function resizecanvas() {
     if ((16 * window.innerHeight / 9) < window.innerWidth) {
         canvas.width = 16 * window.innerHeight / 9;
@@ -35,32 +39,80 @@ function onclick() {
         clicked = true;
     }
 }
+
 function gameloop(ts) {
     if (!lt) {
         lt = ts;
     }
     const dt = (ts - lt) / 1000;
     lt = ts;
-//  ctx.fillStyle = 'black'
-//  ctx.fillRect(0,0,canvas.width,canvas.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (transition > 0) {
+        transition = transition - dt;
+    } else {
+        timer = timer + dt;
+    }
+
     ctx.fillStyle = "white";
     ctx.font = "20px Arial";
-    ctx.fillText(`Canvas: ${canvas.width}x${canvas.height}`, 10, 30);
-    ctx.fillText(`dt: ${dt}`, 10, 60);
-    ctx.fillText(`${fullscreenchanged} , ${clicked}`, 10, 90);
+    if (state === 0) {
+        // start
+        if (timer > 1) {
+            state = 1;
+            timer = 0;
+            transition = 1;
+        } else {
+            ctx.fillText("Start", 10, 30);
+
+        }
+    } else if (state === 1) {
+    // play
+        if (timer > 1) {
+            state = 2;
+            timer = 0;
+            transition = 1;
+        } else {
+            ctx.fillText("Play", 10, 30);
+        }
+    } else {
+        // end
+        if ((timer > 1) && clicked) {
+            state = 0;
+            timer = 0;
+            transition = 1;
+        } else {
+            ctx.fillText("yellow", 10, 30);
+            ctx.fillText(`${timer}`, 10, 60);
+            if (timer > 1) {
+                ctx.fillText("Click To Continue", 10, 90);
+            }
+        }
+    }
+
+    if (transition > 0) {
+        ctx.fillStyle = "white";
+        if (transition > 0.5) {
+            ctx.fillRect(0, 0, canvas.width, 2 * canvas.height * (1 - transition));
+        } else {
+            ctx.fillRect(0, canvas.height * (1 - 2 * transition), canvas.width, canvas.height * 2 * transition);
+        }
+    }
 
 
-    ctx.strokeStyle = "red"
+    ctx.strokeStyle = "red";
     ctx.beginPath();
-    ctx.rect(0,0,canvas.width, canvas.height);
-//    ctx.rect(10,100,100,100);;
+    ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.stroke();
 
+
+
     window.requestAnimationFrame(gameloop);
+
+    clicked = false;
 }
 
 window.addEventListener("resize", resizecanvas);
-canvas.onclick = onclick;
+window.addEventListener("click", onclick);
 resizecanvas();
+transition = 1;
 window.requestAnimationFrame(gameloop);
